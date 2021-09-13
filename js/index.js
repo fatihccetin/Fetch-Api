a6e3d2bf0dc2001c64ede5ad203fe8c34c548586
fatihccetin <fatih.ccetin2@gmail.com>
2021-09-13 18:07:19 +0300

    logo

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const appContainer = document.querySelector(".api"); //html general codes
+
+appContainer.innerHTML = ` <header class="main-header ">
+
+<!-- Top header -->
+
+    <div class="container">
+        <div class="logo">
+        <a href="/"><img alt="logo" src="./img/logo.jPEG" /> </a>
+        </div>
+
+        
+        <div class="search">
+        
+        </div>
+
+        <div class="login-area">
+            <a href="#" class="btn btn-primary">Login <span><i class="fas fa-sign-in-alt"></i></span></a>
+            <a href="#" class="btn btn-primary ml-2">Sign Up <span><i class="fas fa-user-plus"></i></span></a>
+        </div>
+    </div>
+
+</header>`;
+
