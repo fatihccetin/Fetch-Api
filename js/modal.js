260c7602dd7777cc728ea85627aa6e3e4c9d37fa
fatihccetin <fatih.ccetin2@gmail.com>
2021-09-14 01:37:45 +0300

    modal-overlay

--- js/modal.js
+++ js/modal.js
@@ -13,4 +13,10 @@
 
 function alertFunc(){
     document.getElementById('alert').style.display = 'block';
+    document.querySelector(".overlay").style.display="block";
 };
+
+function closeAlert(){
+    document.getElementById('alert').style.display = 'none';
+    document.querySelector(".overlay").style.display="none";
+}
