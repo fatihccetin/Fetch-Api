3917f0f92d5eb1db02b2dacacc729275c8b6792f
fatihccetin <fatih.ccetin2@gmail.com>
2021-09-14 01:18:51 +0300

    modal

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+
+
+
+// var alert = document.querySelector("#alertButton");
+
+// alert.click(function(){
+//     document.querySelector(".modal").style.display="block";
+// });
+
+// function alertFunc(){
+//     var a= document.querySelector(".modal").style.display="block";
+// };
+
+function alertFunc(){
+    document.getElementById('alert').style.display = 'block';
+};
