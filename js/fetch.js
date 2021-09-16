eb2fe2243f78378f1e703ce715f512b60349b9bb
fatihccetin <fatih.ccetin2@gmail.com>
2021-09-16 15:34:19 +0300

    search-area

--- js/fetch.js
+++ js/fetch.js
@@ -1,46 +1,29 @@
-
-let reqHeader = new Headers();
-reqHeader.append('Content-Type', 'text/json');
-let initObject = {
-  method: 'GET', headers: reqHeader,
-};
-
-let value = 5;
-
-const options = {
-    url: 'https://jsonplaceholder.typicode.com/posts',
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json;charset=UTF-8'
-    },
-  };
-  
-  axios(options)
-    .then(response => {
-      console.log(response);
-      const postsContainer = document.querySelector('.posts-container');
-      
-      if (response.status === 200) {
-        for(i=0;i<10;i++){
-          var item = response.data[i];
-          const postWrapper = document.createElement('div');
-          const titleEl = document.createElement('p');
-          const descriptionEl = document.createElement('p');
-          /* const node = document.createTextNode(item.title);
-          titleEl.appendChild(node); */
-          titleEl.className = 'title';
-          descriptionEl.className = 'description';
-          titleEl.innerHTML = `<i>${item.title}</i>`;
-          descriptionEl.innerHTML = `${item.body}`;
-          postWrapper.className = 'post';
-          postWrapper.classList.toggle("wrapper-container"); // to toggle class name. If  classname is exist, remove it.
-          postWrapper.appendChild(titleEl);
-          postWrapper.appendChild(descriptionEl);
-          postsContainer.appendChild(postWrapper);
-          postsContainer.appendChild(postWrapper);
-  
+fetch("https://jsonplaceholder.typicode.com/posts").then(
+    response => {
+       
+        return response.json();
+    }
+).then(
+    responseJson => {
+        
+        let gridElem = ""; 
+        for (let i = 0; i < responseJson.length; i++) {
+            if (i === 6) { //ilk altı tanesini aldık 
+                break;
+            }
+            else {
+               
+                gridElem += `
+                        <div class="container-item">
+                        <img src="img/ciceksepeti.jpg" alt="item${responseJson[i].id}">
+                        <p class="title">${responseJson[i].title}</p>
+                        <p class="data">${responseJson[i].body}</p>
+                        <button class="btn-k">Kaydet</button>
+                        </div>
+                        `;
+            }
+            document.getElementById("grid-contents").innerHTML = gridElem;
         }
         
-      } 
-    });
+    }
+).catch(err => console.log("Hata: ", err));
